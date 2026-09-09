266c163535b9be482cba662b72762ce881d5c365
Yizqq <17306265873@163.com>
2026-09-09 23:31:06 +0800

    feat(app): 添加应用截图生成功能
    - 在 application.yml 中添加配置项
    - 定义 ScreenshotService 接口和实现类
    - 实现异步生成应用截图并更新封面的方法
    - 添加腾讯云COS配置类用于存储截图
    - 创建CosManager对象存储管理器
    - 集成Selenium实现网页截图功能
    - 添加截图工具类WebScreenshotUtils
    - 在应用部署完成后调用截图功能
    - 增加相关的依赖配置

--- src/main/java/com/yizqq/yizcode/service/impl/AppServiceImpl.java
+++ src/main/java/com/yizqq/yizcode/service/impl/AppServiceImpl.java
@@ -24,6 +24,7 @@ import com.yizqq.yizcode.model.vo.AppVO;
 import com.yizqq.yizcode.model.vo.UserVO;
 import com.yizqq.yizcode.service.AppService;
 import com.yizqq.yizcode.service.ChatHistoryService;
+import com.yizqq.yizcode.service.ScreenshotService;
 import com.yizqq.yizcode.service.UserService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -63,6 +64,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
 
     @Resource
     private VueProjectBuilder vueProjectBuilder;
+
+    @Resource
+    private ScreenshotService screenshotService;
 
     @Override
     public AppVO getAppVO(App app) {
@@ -211,8 +215,32 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         updateApp.setDeployedTime(LocalDateTime.now());
         boolean updateResult = this.updateById(updateApp);
         ThrowUtils.throwIf(!updateResult, ErrorCode.OPERATION_ERROR, "更新应用部署信息失败");
-        // 10. 构建应用访问 URL
-        return String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        String appDeployUrl = String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);        // 11. 异步生成截图并且更新应用封面
+        generateAppScreenshotAsync(appId, appDeployUrl);
+        return appDeployUrl;
+    }
+
+
+
+    /**
+     * 异步生成应用截图并更新封面
+     *
+     * @param appId  应用ID
+     * @param appUrl 应用访问URL
+     */
+    @Override
+    public void generateAppScreenshotAsync(Long appId, String appUrl) {
+        // 使用虚拟线程并执行
+        Thread.startVirtualThread(() -> {
+            // 调用截图服务生成截图并上传
+            String screenshotUrl = screenshotService.generateAndUploadScreenshot(appUrl);
+            // 更新数据库的封面
+            App updateApp = new App();
+            updateApp.setId(appId);
+            updateApp.setCover(screenshotUrl);
+            boolean updated = this.updateById(updateApp);
+            ThrowUtils.throwIf(!updated, ErrorCode.OPERATION_ERROR, "更新应用封面字段失败");
+        });
     }
 
     /**
